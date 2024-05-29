d0d8bbb982d46dd3f526dcb5aa47712f0c4776d0
Inzimamul Ansari <inzamamansari360@gmail.com>
2024-05-30 03:34:02 +0530

    NSDL Swift ListView Package

--- Package.swift
+++ Package.swift
@@ -5,19 +5,18 @@ import PackageDescription
 
 let package = Package(
     name: "NSDLSwiftPackage",
+    platforms: [.iOS(.v14)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NSDLSwiftPackage",
             targets: ["NSDLSwiftPackage"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/Alamofire/Alamofire.git", .upToNextMajor(from: "5.9.1"))
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NSDLSwiftPackage"),
-        .testTarget(
-            name: "NSDLSwiftPackageTests",
-            dependencies: ["NSDLSwiftPackage"]),
+            name: "NSDLSwiftPackage",
+            dependencies: ["Alamofire"]),
     ]
 )
--- Sources/NSDLSwiftPackage/NSDLSwiftPackage.swift
+++ Sources/NSDLSwiftPackage/NSDLSwiftPackage.swift
@@ -1,2 +1,66 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import SwiftUI
+import Alamofire
+
+#if canImport(UIKit)
+@available(iOS 14.0, *)
+public struct EmailListView: View {
+    @State private var users: [User] = []
+    @State private var selectedEmail: String?
+    @Environment(\.presentationMode) var presentationMode
+    let completion: (String) -> Void
+
+    public init(completion: @escaping (String) -> Void) {
+        self.completion = completion
+    }
+
+    public var body: some View {
+        VStack {
+            List(users, id: \.id) { user in
+                Text(user.email)
+            }
+            Spacer()
+            Button("Return To App") {
+                returnFirstEmail()
+            }
+            .frame(width: 150, height: 50)
+            .border(Color.black)
+            .padding(.bottom, 10)
+        }
+        .onAppear(perform: callAPI)
+    }
+
+    private func callAPI() {
+        let url = "https://reqres.in/api/users?page=1"
+        AF.request(url).responseDecodable(of: UserResponse.self) { response in
+            switch response.result {
+            case .success(let userResponse):
+                self.users = userResponse.data
+#if DEBUG
+                print(userResponse.data)
+#endif
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+
+    private func returnFirstEmail() {
+        if let firstUser = users.first {
+            completion(firstUser.email)
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+}
+
+struct User: Decodable {
+    let id: Int
+    let email: String
+}
+
+struct UserResponse: Decodable {
+    let data: [User]
+}
+#endif
